8997dd1ee721b7f01d70436963596095a1320dd6
Anthony Ng <newyork.anthonyng@gmail.com>
2020-12-03 17:14:59 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({ ok: "foo" });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
